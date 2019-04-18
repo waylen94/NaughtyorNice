ad1d6beb832f328c81de307729c1473d385237ae
Weilun Liu <763027562@qq.com>
2019-04-18 13:54:40 +0000

    initial the home page coresponded file

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+  <footer class="footer bg-light py-5">
+    <div class="container">
+    
+    <div class="float-left small text-center text-muted">
+    <p>Powered by Team-MINT Copyright &copy; 2019</p>
+    </div>
+
+    <p class="float-right"><a href="mailto:name@email.com">Contact us</a></p>
+  </footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Homepage')
+
+@section('content')
+  <h1>This is home page</h1>
+@stop
