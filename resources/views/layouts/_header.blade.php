adfadb211e2e517708bf7fb5f585a7aa64d88037
Weilun Liu <763027562@qq.com>
2019-05-20 01:16:40 +0000

    changing project name

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,7 +2,7 @@
   <div class = "container">
   <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-static-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="{{ url('/') }}">Naughty or Nice</a>
+      <a class="navbar-brand js-scroll-trigger" href="{{ url('/') }}">Network Level Defense for IoT</a>
       
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
