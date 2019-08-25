c944499dd50aee7ce09eba1da65d76b5d1e3b6af
Weilun Liu <763027562@qq.com>
2019-08-25 07:05:39 +0000

    accomplish the basis features

--- public/js/app.js
+++ public/js/app.js
@@ -49472,59 +49472,77 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports) {
 
 if (document.getElementById('deco7861')) {
-  vis_dataset_id_label = [];
+  var json_data = json_data_diagram; //After we have corresponded Json File ex. json_data 
 
-  for (var j = 0, jl = json_data_diagram.node.length; j < jl; j++) {
+  vis_dataset_id_label = []; // store nodes info
+  //Seperate Node from json
+
+  for (var j = 0, jl = json_data.node.length; j < jl; j++) {
     vis_dataset_id_label.push({
-      id: json_data_diagram.node[j][0],
-      label: json_data_diagram.node[j][1]
+      id: json_data.node[j][0],
+      label: json_data.node[j][1]
     });
   }
 
   var nodes = new vis.DataSet(vis_dataset_id_label); // create an array with edges
 
-  vis_dataset_connection = [];
+  vis_dataset_connection = []; // store edge info
+  //seperate edge from json
 
-  for (var j = 0, jl = json_data_diagram.edge.length; j < jl; j++) {
+  for (var j = 0, jl = json_data.edge.length; j < jl; j++) {
     vis_dataset_connection.push({
-      from: json_data_diagram.edge[j][0],
-      to: json_data_diagram.edge[j][1],
+      from: json_data.edge[j][0],
+      to: json_data.edge[j][1],
       arrows: 'to'
     });
   }
 
   var edges = new vis.DataSet(vis_dataset_connection); // create a network
+  //Capture the container where exhibited the diagram
 
   var container = document.getElementById('deco7861');
-  nodes.update({
-    id: 3,
-    color: "rgba(245,70,15)"
-  });
-  nodes.update({
-    id: 7,
-    color: "rgba(245,70,15)"
-  });
-  nodes.update({
-    id: 8,
-    color: "rgba(245,70,15)"
-  });
-  nodes.update({
-    id: 100,
-    color: "#7BE141"
-  });
-  nodes.update({
-    id: 102,
-    color: "#7BE141"
-  });
-  nodes.update({
-    id: 106,
-    color: "#7BE141"
-  });
-  nodes.update({
-    id: 500,
-    color: "rgba(151,151,151)"
-  }); //    nodes.update({id:500, color: {background:'pink', border:'purple'}});
-  // provide the data in the vis format
+
+  for (i = 0; i < vis_dataset_id_label.length; i++) {
+    var id = vis_dataset_id_label[i].id;
+    var str = vis_dataset_id_label[i].label;
+
+    if (str.indexOf("attacker") > -1) {
+      //detecting attacker node and change the color to grey
+      nodes.update({
+        id: Number(id),
+        color: "rgba(151,151,151)"
+      });
+    } else if (str.indexOf("server") > -1) {
+      //detecting server node
+      //if it is decoy then green
+      if (str.indexOf("decoy") > -1) {
+        nodes.update({
+          id: Number(id),
+          color: "#7BE141"
+        }); //if it is real then red
+      } else {
+        nodes.update({
+          id: Number(id),
+          color: "rgba(245,70,15)"
+        });
+      }
+    } else if (str.indexOf("target") > -1) {
+      //detecting target node and change to red
+      //if it is decoy then green
+      if (str.indexOf("decoy") > -1) {
+        nodes.update({
+          id: Number(id),
+          color: "#7BE141"
+        }); //if it is real then red
+      } else {
+        nodes.update({
+          id: Number(id),
+          color: "rgba(245,70,15)"
+        });
+      }
+    }
+  } // provide the data in the vis format
+
 
   var data = {
     nodes: nodes,
@@ -49533,9 +49551,6 @@ if (document.getElementById('deco7861')) {
   var options = {}; // initialize your network!
 
   var network = new vis.Network(container, data, options);
-  network.focus("3");
-  network.focus("7");
-  network.focus("8");
   network.redraw();
 }
 
--- resources/js/deco7861.js
+++ resources/js/deco7861.js
@@ -1,49 +1,84 @@
 
 if(document.getElementById('deco7861')){
-	
-	vis_dataset_id_label = []
-	
-	for(var j=0,jl=json_data_diagram.node.length;j < jl;j++){
-        vis_dataset_id_label.push({id: json_data_diagram.node[j][0],label: json_data_diagram.node[j][1]});
-    }
-	
-	
-	var nodes = new vis.DataSet(vis_dataset_id_label);
+	var json_data = json_data_diagram;
+	//After we have corresponded Json File ex. json_data 
+	  vis_dataset_id_label = []; // store nodes info
+	//Seperate Node from json
+	  for (var j = 0, jl = json_data.node.length; j < jl; j++) {
+	    vis_dataset_id_label.push({
+	      id: json_data.node[j][0],
+	      label: json_data.node[j][1]
+	    });
+	  }
 
-    // create an array with edges
-	vis_dataset_connection = []
-	for(var j=0,jl=json_data_diagram.edge.length;j < jl;j++){
-        vis_dataset_connection.push({from: json_data_diagram.edge[j][0],to: json_data_diagram.edge[j][1], arrows:'to'});
-    }
-    var edges = new vis.DataSet(vis_dataset_connection);
+	  var nodes = new vis.DataSet(vis_dataset_id_label); // create an array with edges
 
-    // create a network
-    var container = document.getElementById('deco7861');
-    
-    
-    nodes.update({id:3, color:"rgba(245,70,15)"});
-    nodes.update({id:7, color:"rgba(245,70,15)"});
-    nodes.update({id:8, color:"rgba(245,70,15)"});
-    nodes.update({id:100, color:"#7BE141"});
-    nodes.update({id:102, color:"#7BE141"});
-    nodes.update({id:106, color:"#7BE141"});
-    nodes.update({id:500, color:"rgba(151,151,151)"});
-//    nodes.update({id:500, color: {background:'pink', border:'purple'}});
-    // provide the data in the vis format
-    var data = {
-        nodes: nodes,
-        edges: edges
-    };
-    var options = {
+	  vis_dataset_connection = []; // store edge info
+	//seperate edge from json
+	  for (var j = 0, jl = json_data.edge.length; j < jl; j++) {
+	    vis_dataset_connection.push({
+	      from: json_data.edge[j][0],
+	      to: json_data.edge[j][1],
+	      arrows: 'to',
+	    });
+	  }
 
-    };
-    // initialize your network!
-    var network = new vis.Network(container, data, options);
-    network.focus("3");
-    network.focus("7");
-    network.focus("8");
-    network.redraw();
-}
+	  var edges = new vis.DataSet(vis_dataset_connection); // create a network
+	  //Capture the container where exhibited the diagram
+	  var container = document.getElementById('deco7861');
+	  
+
+	  for (i = 0; i < vis_dataset_id_label.length; i++) { 
+		  var id = vis_dataset_id_label[i].id;
+		  var str = vis_dataset_id_label[i].label;	
+		  if(str.indexOf("attacker")>-1){
+			//detecting attacker node and change the color to grey
+			  nodes.update({
+				    id: Number(id),
+				    color: "rgba(151,151,151)"
+				  }); 
+		  }else if(str.indexOf("server")>-1){
+			//detecting server node
+			  //if it is decoy then green
+			  if (str.indexOf("decoy")>-1){
+				  nodes.update({
+					    id: Number(id),
+					    color: "#7BE141"
+					  }); 
+				  //if it is real then red
+			  }else{
+				  nodes.update({
+					    id: Number(id),
+					    color: "rgba(245,70,15)"
+					  }); 
+			  }
+		  }else if(str.indexOf("target")>-1){
+			//detecting target node and change to red
+			  //if it is decoy then green
+			  if (str.indexOf("decoy")>-1){
+				  nodes.update({
+					    id: Number(id),
+					    color: "#7BE141"
+					  }); 
+				  //if it is real then red
+			  }else{
+				  nodes.update({
+					    id: Number(id),
+					    color: "rgba(245,70,15)"
+					  }); 
+			  }
+		  }
+		}
+
+		  // provide the data in the vis format
+		  var data = {
+		    nodes: nodes,
+		    edges: edges
+		  };	  
+		  var options = {}; // initialize your network!
+		  var network = new vis.Network(container, data, options);
+		  network.redraw();
+	}
 						
 						
 						
